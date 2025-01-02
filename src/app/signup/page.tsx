6b1a7a64279ecd87a203341bca5a60f001649f3c
deadsoulartist <deadsoulartist@gmail.com>
2025-01-02 14:10:01 +0530

    feat: verify email for user flow

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,6 +5,8 @@ import Header from "../_components/header";
 import { api } from "~/trpc/react";
 
 export default function Signup() {
+  const [showVerificationUI, setShowVerificationUI] = useState(false);
+
   const signupMutation = api.user.signup.useMutation(); // useMutation instead of useSuspenseQuery
   const verifyCodeMutation = api.user.verifyCode.useMutation();
 
@@ -14,7 +16,11 @@ export default function Signup() {
     password: string;
   }) => {
     signupMutation.mutate(data, {
-      onSuccess: (res) => alert(res.message),
+      onSuccess: (res) => {
+        alert(res.message);
+        setShowVerificationUI(true);
+      },
+      onError: (err) => alert(err.message),
     });
   };
 
@@ -25,6 +31,7 @@ export default function Signup() {
   }) => {
     verifyCodeMutation.mutate(data, {
       onSuccess: (res) => alert(res.message),
+      onError: (err) => alert(err.message),
     });
   };
 
@@ -33,12 +40,17 @@ export default function Signup() {
     email: "",
     password: "",
   });
+  const [code, setCode] = useState(null);
   const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
     setErrors({ ...errors, [name]: "" }); // Clear error when typing
+  };
+
+  const handleCodeChange = (e) => {
+    setCode(e.target.value);
   };
 
   const validate = () => {
@@ -68,83 +80,131 @@ export default function Signup() {
     }
   };
 
+  const handleVerify = (e) => {
+    e.preventDefault();
+    // const validationErrors = validate();
+    // if (Object.keys(validationErrors).length > 0) {
+    //   setErrors(validationErrors);
+    // } else {
+    const { name, email, password } = formData;
+    onVerify({
+      email,
+      code,
+      password,
+    });
+    alert("Sign-up successful!");
+    // }
+  };
+
   return (
     <>
       <Header />
       <main className="flex h-[100vh] items-center justify-center bg-gray-100">
-        <div className="w-full max-w-md rounded-md bg-white p-6 shadow-md">
-          <h2 className="mb-4 text-center text-2xl font-bold">
-            Create your account
-          </h2>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-              <label htmlFor="name" className="block font-medium">
-                Name
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                value={formData.name}
-                onChange={handleChange}
-                className={`w-full rounded-md border px-3 py-2 ${
-                  errors.name ? "border-red-500" : "border-gray-300"
-                }`}
-              />
-              {errors.name && (
-                <p className="text-sm text-red-500">{errors.name}</p>
-              )}
+        {showVerificationUI ? (
+          <>
+            <div className="w-full max-w-md rounded-md bg-white p-6 shadow-md">
+              <h2 className="mb-4 text-center text-2xl font-bold">
+                Verify your email
+              </h2>
+              <form onSubmit={handleVerify} className="space-y-4">
+                <div>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={code}
+                    onChange={handleCodeChange}
+                    className={`"border-gray-300" w-full rounded-md border px-3 py-2`}
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full rounded-md bg-black py-2 text-white"
+                >
+                  Verify
+                </button>
+              </form>
             </div>
-            <div>
-              <label htmlFor="email" className="block font-medium">
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                value={formData.email}
-                onChange={handleChange}
-                className={`w-full rounded-md border px-3 py-2 ${
-                  errors.email ? "border-red-500" : "border-gray-300"
-                }`}
-              />
-              {errors.email && (
-                <p className="text-sm text-red-500">{errors.email}</p>
-              )}
+          </>
+        ) : (
+          <>
+            {" "}
+            <div className="w-full max-w-md rounded-md bg-white p-6 shadow-md">
+              <h2 className="mb-4 text-center text-2xl font-bold">
+                Create your account
+              </h2>
+              <form onSubmit={handleSubmit} className="space-y-4">
+                <div>
+                  <label htmlFor="name" className="block font-medium">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    className={`w-full rounded-md border px-3 py-2 ${
+                      errors.name ? "border-red-500" : "border-gray-300"
+                    }`}
+                  />
+                  {errors.name && (
+                    <p className="text-sm text-red-500">{errors.name}</p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="email" className="block font-medium">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    className={`w-full rounded-md border px-3 py-2 ${
+                      errors.email ? "border-red-500" : "border-gray-300"
+                    }`}
+                  />
+                  {errors.email && (
+                    <p className="text-sm text-red-500">{errors.email}</p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="password" className="block font-medium">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    className={`w-full rounded-md border px-3 py-2 ${
+                      errors.password ? "border-red-500" : "border-gray-300"
+                    }`}
+                  />
+                  {errors.password && (
+                    <p className="text-sm text-red-500">{errors.password}</p>
+                  )}
+                </div>
+                <button
+                  type="submit"
+                  className="w-full rounded-md bg-black py-2 text-white"
+                >
+                  CREATE ACCOUNT
+                </button>
+              </form>
+              <p className="mt-4 text-center text-sm">
+                Have an Account?{" "}
+                <a href="#" className="text-blue-600">
+                  LOGIN
+                </a>
+              </p>
             </div>
-            <div>
-              <label htmlFor="password" className="block font-medium">
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                value={formData.password}
-                onChange={handleChange}
-                className={`w-full rounded-md border px-3 py-2 ${
-                  errors.password ? "border-red-500" : "border-gray-300"
-                }`}
-              />
-              {errors.password && (
-                <p className="text-sm text-red-500">{errors.password}</p>
-              )}
-            </div>
-            <button
-              type="submit"
-              className="w-full rounded-md bg-black py-2 text-white"
-            >
-              CREATE ACCOUNT
-            </button>
-          </form>
-          <p className="mt-4 text-center text-sm">
-            Have an Account?{" "}
-            <a href="#" className="text-blue-600">
-              LOGIN
-            </a>
-          </p>
-        </div>
+          </>
+        )}
       </main>
     </>
   );
